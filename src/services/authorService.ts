b330fb02d9f2f208f6b2b56049c32615380f1d8e
Deepak Tomar <30664265+deepaktomar2031@users.noreply.github.com>
2025-01-17 22:48:20 +0100

    Implement authentication flow with login and private routes

--- src/services/authorService.ts
+++ src/services/authorService.ts
@@ -1,27 +1,27 @@
 import axios from 'axios'
+import { API_BASE_URL } from 'src/utils'
 import { ICreateAuthor } from 'src/types'
 
 // Create new author
 export const createAuthor = async (author: ICreateAuthor) => {
-  const response = await axios.post(
-    `${process.env.REACT_APP_ARTICLE_MANAGEMENT_API_BASE_URL}/author`,
-    author
-  )
+  const response = await axios.post(`${API_BASE_URL}/author`, author)
   return response.data
 }
 
 // Fetch all authors
 export const getAllAuthors = async () => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_ARTICLE_MANAGEMENT_API_BASE_URL}/authors`
-  )
+  const token = localStorage.getItem('access_token')
+  const response = await axios.get(`${API_BASE_URL}/authors`, {
+    headers: { Authorization: `Bearer ${token}` },
+  })
   return response.data
 }
 
 // Fetch author by ID
 export const getAuthorById = async (id: number) => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_ARTICLE_MANAGEMENT_API_BASE_URL}/author/${id}`
-  )
+  const token = localStorage.getItem('access_token')
+  const response = await axios.get(`${API_BASE_URL}/author/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  })
   return response.data
 }
