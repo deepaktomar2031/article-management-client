6fde1e8ae6ccc33a44384b5407767f168a397fd7
Deepak Tomar <30664265+deepaktomar2031@users.noreply.github.com>
2025-01-19 18:09:29 +0100

    Refactor article, author, and comment

--- src/components/Comment/CommentList.tsx
+++ src/components/Comment/CommentList.tsx
@@ -5,7 +5,7 @@ import { IComment } from 'src/types'
 import { LogErrorMessage } from 'src/utils'
 
 const CommentList: React.FC = () => {
-  const [Comments, setComments] = useState<IComment[]>([])
+  const [comments, setComments] = useState<IComment[]>([])
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -24,18 +24,20 @@ const CommentList: React.FC = () => {
     fetchComments()
   }, [])
 
+  const commentHeaders = ['ID', 'Author ID', 'Article ID', 'Content', 'Created At']
+
   const renderCommentRows = (comment: IComment) => {
-    const CommentData = [
-      { label: 'ID', value: comment.id },
-      { label: 'Author ID', value: comment.authorId },
-      { label: 'Article ID', value: comment.articleId },
-      { label: 'Content', value: comment.content },
-      { label: 'Created At', value: new Date(comment.createdAt).toLocaleString() },
+    const commentValues = [
+      comment.id,
+      comment.authorId,
+      comment.articleId,
+      comment.content,
+      new Date(comment.createdAt).toLocaleString(),
     ]
 
-    return CommentData.map((item, index) => (
+    return commentValues.map((value, index) => (
       <td key={index} style={{ border: '3px solid black', padding: '8px' }}>
-        {item.value}
+        {value}
       </td>
     ))
   }
@@ -46,15 +48,15 @@ const CommentList: React.FC = () => {
       <table style={{ border: '3px solid black', width: '100%', borderCollapse: 'collapse' }}>
         <thead>
           <tr>
-            <th style={{ border: '3px solid black', padding: '8px' }}>ID</th>
-            <th style={{ border: '3px solid black', padding: '8px' }}>Author ID</th>
-            <th style={{ border: '3px solid black', padding: '8px' }}>Article ID</th>
-            <th style={{ border: '3px solid black', padding: '8px' }}>Content</th>
-            <th style={{ border: '3px solid black', padding: '8px' }}>Created At</th>
+            {commentHeaders.map((header, index) => (
+              <th key={index} style={{ border: '3px solid black', padding: '8px' }}>
+                {header}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {Comments.map((comment) => (
+          {comments.map((comment) => (
             <tr key={comment.id}>{renderCommentRows(comment)}</tr>
           ))}
         </tbody>
